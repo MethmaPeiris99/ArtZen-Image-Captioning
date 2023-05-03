038d9ec6db08612c343ee38f21a2a128dc208bd6
Methma Peiris <61134846+MethmaPeiris99@users.noreply.github.com>
2023-05-03 09:39:12 +0530

    Build a method to create image captions

--- final.py
+++ final.py
@@ -42,6 +42,16 @@ def get_colour_names(detected_colours):
 
     return common_colour_names
 
+def build_image_caption(count_objects, detected_colours):
+    if len(count_objects) == 0:
+        return f"{detected_colours}, no objects"
+    else:
+        for object_name, object_count in count_objects.items():
+            if object_count > 1:
+                return f"{detected_colours}, {object_count} {object_name}s"
+            else:
+                return f"{detected_colours}, {object_count} {object_name}"
+
 
 execution_path = os.getcwd()
 detector = ObjectDetection()  # Create an instance of ObjectDetection()
@@ -57,7 +67,7 @@ image_caption_data = []
 
 image_folder_directory = r"D:\retinanet_resnet50_fpn_coco-eeacb38b\test-paintings"
 
-columnNames = ['Image File', 'Colours', 'Objects']
+columnNames = ['Image File', 'Colours', 'Objects', 'Image Caption']
 
 with open('wikiart-captions.csv', 'w') as wikiArtCaptionFile:
     csvWriter = csv.writer(wikiArtCaptionFile)
@@ -86,7 +96,8 @@ with open('wikiart-captions.csv', 'w') as wikiArtCaptionFile:
 
         detected_colour_names = get_colour_names(detectedColours)
         image_file_name = image
-        image_caption_data = [image_file_name, detected_colour_names, detectedObjects]
+        image_caption = build_image_caption(countObjects, detected_colour_names)
+        image_caption_data = [image_file_name, detected_colour_names, detectedObjects, image_caption]
         print(image_caption_data, "\n")
 
         csvWriter.writerow(image_caption_data)
